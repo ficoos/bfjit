dcd7c924ca8b32fc5269a14c3b70e69d3e17eb8e
Saggi Mizrahi <saggi@mizrahi.cc>
2018-04-09 22:56:25 +0300

    Add compiler

---
+++ cmd/bfc/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"../../bf"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"llvm.org/llvm/bindings/go/llvm"
+	"os"
+)
+
+var outputFile = flag.String("o", "out.ll", "Output file")
+
+func main() {
+	flag.Parse()
+	if flag.NArg() != 1 {
+		fmt.Printf("Usage: %s [options..] <input>\n", os.Args[0])
+		flag.PrintDefaults()
+		os.Exit(-1)
+	}
+
+	mod := llvm.NewModule("bf")
+
+	putcharFuncType := llvm.FunctionType(llvm.VoidType(), []llvm.Type{llvm.Int8Type()}, false)
+	llvm.AddFunction(mod, "bf_putchar", putcharFuncType)
+	getcharFuncType := llvm.FunctionType(llvm.Int8Type(), []llvm.Type{}, false)
+	llvm.AddFunction(mod, "bf_getchar", getcharFuncType)
+	memsetFuncType := llvm.FunctionType(llvm.PointerType(llvm.Int64Type(), 0), []llvm.Type{llvm.PointerType(llvm.Int8Type(), 0), llvm.Int8Type(), llvm.Int64Type()}, false)
+	llvm.AddFunction(mod, "memset", memsetFuncType)
+
+	if res, err := ioutil.ReadFile(flag.Arg(0)); err != nil {
+		fmt.Printf("Could not read input file: %s", err)
+		os.Exit(-1)
+	} else if err := bf.Compile(string(res), mod); err != nil {
+		fmt.Printf("Could not compile: %s", err)
+		os.Exit(-1)
+	}
+
+	f, err := os.Create(*outputFile)
+	if err != nil {
+		fmt.Printf("Cant open output file: %s", err)
+		os.Exit(-1)
+	}
+	defer f.Close()
+	if err := llvm.WriteBitcodeToFile(mod, f); err != nil {
+		fmt.Printf("Failed to write bitcode to file: %s", err)
+		os.Exit(-1)
+	}
+}
---
+++ cmd/bfjit/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"../../bf"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"llvm.org/llvm/bindings/go/llvm"
+	"os"
+)
+
+func main() {
+	flag.Parse()
+	if flag.NArg() != 1 {
+		fmt.Printf("Usage: %s <input>\n", os.Args[0])
+		flag.PrintDefaults()
+		os.Exit(-1)
+	}
+
+	llvm.LinkInMCJIT()
+	if err := llvm.InitializeNativeTarget(); err != nil {
+		panic(err)
+	}
+
+	if err := llvm.InitializeNativeAsmPrinter(); err != nil {
+		panic(err)
+	}
+
+	mod := llvm.NewModule("bf")
+	opts := llvm.MCJITCompilerOptions{}
+	opts.SetMCJITCodeModel(llvm.CodeModelJITDefault)
+	opts.SetMCJITOptimizationLevel(3)
+	engine, err := llvm.NewMCJITCompiler(mod, opts)
+	if err != nil {
+		panic(err)
+	}
+
+	putcharFuncType := llvm.FunctionType(llvm.VoidType(), []llvm.Type{llvm.Int8Type()}, false)
+	llvm.AddFunction(mod, "bf_putchar", putcharFuncType)
+	getcharFuncType := llvm.FunctionType(llvm.Int8Type(), []llvm.Type{}, false)
+	llvm.AddFunction(mod, "bf_getchar", getcharFuncType)
+	memsetFuncType := llvm.FunctionType(llvm.PointerType(llvm.Int64Type(), 0), []llvm.Type{llvm.PointerType(llvm.Int8Type(), 0), llvm.Int8Type(), llvm.Int64Type()}, false)
+	llvm.AddFunction(mod, "memset", memsetFuncType)
+
+	if res, err := ioutil.ReadFile(flag.Arg(0)); err != nil {
+		fmt.Printf("Could not read input file: %s", err)
+		os.Exit(-1)
+	} else if err := bf.Compile(string(res), mod); err != nil {
+		fmt.Printf("Could not compile: %s", err)
+		os.Exit(-1)
+	}
+
+	engine.RunStaticConstructors()
+	engine.RunFunction(mod.NamedFunction("main"), []llvm.GenericValue{})
+	engine.RunStaticDestructors()
+}
